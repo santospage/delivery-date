2d06793ec109de8d37fad60a683e68f652e0196f
santospage <alessandro.siga@gmail.com>
2022-02-03 00:12:11 -0300

    Add initial project delivery_date for backend

---
+++ backend/delivery_date/src/test/java/br/com/santospage/delivery_date/DeliveryDateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.santospage.delivery_date;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
