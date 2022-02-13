c3cb3366f875d79eb4bcc92b180708a223adcd1c
Alessandro <alessandro.siga@gmail.com>
2022-02-13 20:03:31 -0300

    Delete project incorrect

--- backend/delivery_date/src/main/java/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.santospage.delivery_date.models.Branch;
-
-public interface OrderRepository extends JpaRepository<Branch, Long>{
-
-}
