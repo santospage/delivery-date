e277aa4cfa89854441a1c1bd37415b30a4e20c5d
santospage <alessandro.siga@gmail.com>
2022-06-14 16:34:13 -0300

    Added filter component e alter style for scss

--- frontend/delivery_date/src/app/app-routing.module.ts
+++ frontend/delivery_date/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FiltersComponent } from './filters/filters.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'filters', pathMatch: 'full' },
+  { path: 'filters', component: FiltersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/delivery_date/src/app/app.module.ts
+++ frontend/delivery_date/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FiltersComponent } from './filters/filters.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
