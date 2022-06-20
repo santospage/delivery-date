1efbac7f96987f54d4844fcf4ce5b5f4a0baef76
santospage <alessandro.siga@gmail.com>
2022-06-20 17:03:11 -0300

    Added branch model, branch service and select for filters

---
+++ frontend/delivery_date/src/app/services/branch.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Branch } from '../models/branch.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BranchService {
+  private listaBranches: any[];
+  private url = 'http://localhost:8080/deliverydate/branch';
+
+  constructor(private httpClient: HttpClient) {
+    this.listaBranches = [];
+  }
+
+  get branches(): any[] {
+    return this.listaBranches;
+  }
+
+  GelAllBranches(): Observable<Branch[]> {
+    return this.httpClient.get<Branch[]>(this.url);
+  }
+}
